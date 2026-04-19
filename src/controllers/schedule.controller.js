a028763fb9dfad40a2004043edf86926a34955fc
drtalal1967 <167581810+drtalal1967@users.noreply.github.com>
2026-04-19 16:02:25 +0300

    Update schedule.controller.js

--- src/controllers/schedule.controller.js
+++ src/controllers/schedule.controller.js
@@ -32,6 +32,12 @@ const createManySchedules = async (req, res, next) => {
 
 const getSchedules = async (req, res, next) => {
   try {
+    const role =
+  req.user?.role?.name?.toUpperCase() ||
+  req.user?.role?.toUpperCase() ||
+  req.user?.roleName?.toUpperCase();
+
+const isPrivileged = ["ADMIN", "SECRETARY"].includes(role);
     const { start, end, month } = req.query;
 
     // MANDATORY VALIDATION: Month or (Start and End)
